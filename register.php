ec3ce024c53ff3b95318c42ddf95dca3c71de52c
Sarmatt <artabc98@gmail.com>
2024-03-20 17:45:25 +0200

    Added main site

---
+++ register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>LiteralNest Rustcraft</title>
+  <link rel="stylesheet" type="text/css" href="registerStyle.css">
+  <link href='https://fonts.googleapis.com/css?family=Metal Mania' rel='stylesheet'>
+  <link href='https://fonts.googleapis.com/css?family=Koulen' rel='stylesheet'>
+</head>
+<body>
+
+  <header>
+    <div class="company-data">
+      <img class="company-data-logo" src="img/CompanyLogo.png" alt="Logo">
+      <h1 class="company-data-title">LITERALNEST</h1>
+    </div>  
+  </header>
+
+<div class="register-form">
+  <h2>Register</h2>
+  <form>
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username"><br>
+    <label for="email">Email:</label><br>
+    <input type="email" id="email" name="email"><br>
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password"><br>
+    <label for="confirm-password">Confirm Password:</label><br>
+    <input type="password" id="confirm-password" name="confirm-password"><br>
+    <input type="submit" value="Register">
+  </form>
+  <p>Already have an account? <a href="/authorization.php">Login here</a></p>
+</div>
+
+</body>
+</html>
